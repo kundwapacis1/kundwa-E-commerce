306929beefa7aa28c034c9f89c735ac8b2fe2300
kundwa pacis1 <gihozokundwa250@gmail.com>
2025-11-19 17:27:29 +0200

    addition of cartcontroller.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,3 +4,31 @@ export const getCart = async (req, res) =>{
     const cart = await cart.finfOne({ user: req.user._id }).populate("items.populate");
     res.json(cart);
 };
+
+export const addTocart = async (req, res) => {
+    const { product, quantity} = req.body;
+    let cart = await cart.findOne({ user: req.user._id});
+
+    if (!cart) {
+        cart = await cart.create({user: req.user._id, items: [{product, quantity}] });
+    } else {
+        const itemIndex = cart.items.findIndex(item.product.toString() === product);
+        if (itemIndex > -1) {
+            cart.items[itemIndex].quantity += quantity;
+        } else {
+            cart.items.push({ product, quantity });
+        }
+        await cart.save();
+    }
+    res.json(cart);
+};
+
+export const removeFromCart = async (req, res) => {
+    const { productId } = req.params;
+    const cart =  await cart.findOne({ user: req.user._id });
+
+    if (!cart) return res.status(404).json({ message: "Cart not found"});
+    cart.items = cart.items.filter(item => item.product.toString() !== productId);
+    await cart.save();
+    res.json(cart);
+}
---
+++ routes/cartRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getCart, addToCart, removeFromCart } from "../controllers/cartController.js";
+import  { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.use(protect);
+
+router.get("/", getCart);
+router.post("/add", addToCart);
+router.delete("/remove/:productId", removeFromCart);
+
+export default router;
